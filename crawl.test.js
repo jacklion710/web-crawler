b5d5c926391a05124bcd3c45aff796890ee90a5f
jacklion710 <jack.lion710@gmail.com>
2024-04-02 00:40:10 -0400

    Unit tests for normalizeURL()

---
+++ crawl.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@jest/globals')
+const { normalizeURL } = require('./crawl.js')
+
+test('inputs a URL with https:// and a trailing / and normalizes it', () => {
+    expect(normalizeURL('https://blog.boot.dev/path/').toBe('blog.boot.dev/path'))
+})
+
+test('inputs a URL with https:// and normalizes it', () => {
+    expect(normalizeURL('https://blog.boot.dev/path').toBe('blog.boot.dev/path'))
+})
+
+test('inputs a URL with http:// and a trailing / and normalizes it', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/').toBe('blog.boot.dev/path'))
+})
+
+test('inputs a URL with http:// and normalizes it', () => {
+    expect(normalizeURL('http://blog.boot.dev/path').toBe('blog.boot.dev/path'))
+})
+
+test('inputs a URL with a token and normalizes it', () => {
+    expect(normalizeURL('http://blog.boot.dev/path/?d=e').toBe('blog.boot.dev/path'))
+})
