b53b0d6033d4d92594739bac4ac84d6e05203c5b
jacklion710 <jack.lion710@gmail.com>
2024-04-04 00:05:18 -0400

    getURLsFromHTML() function implementation & tests

--- crawl.js
+++ crawl.js
@@ -1,9 +1,38 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(urlString) {
     const url = new URL(urlString)
     const hostPath = `${url.hostname}${url.pathname}`
     return hostPath.endsWith('/') ? hostPath.slice(0, -1) : hostPath
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for (linkElement of linkElements) {
+        if(linkElement.href.slice(0, 1) === '/') {
+            // relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)
+                urls.push(urlObj.href)
+            } catch(err) {
+                console.log('error with relative URL:', err.message)
+            }
+        } else {
+            // absolute
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href)
+            } catch(err) {
+                console.log('error with absolute URL:', err.message)
+            }
+        }
+    }
+    return urls
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
+// Tests for normalizeURL()
 test('inputs a URL with https:// and a trailing / and normalizes it', () => {
   expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path')
 })
@@ -20,3 +21,81 @@ test('inputs a URL with http:// and normalizes it', () => {
 test('inputs a URL with a token and normalizes it', () => {
   expect(normalizeURL('http://blog.boot.dev/path/?d=e')).toBe('blog.boot.dev/path')
 })
+
+// Tests for getURLsFromHTML()
+
+test('gets multiple anchor tags from an HTML document', () => {
+  const html = `
+    <html>
+      <body>
+        <a href="http://blog.boot.dev/path1">Link 1</a>
+        <p>Some text</p>
+        <a href="http://blog.boot.dev/path2">Link 2</a>
+        <a href="http://blog.boot.dev/path3">Link 3</a>
+      </body>
+    </html>
+  `;
+  expect(getURLsFromHTML(html)).toEqual([
+    'http://blog.boot.dev/path1',
+    'http://blog.boot.dev/path2',
+    'http://blog.boot.dev/path3',
+  ]);
+});
+
+test('handles nested anchor tags', () => {
+  const html = `
+    <html>
+      <body>
+        <div>
+          <a href="http://blog.boot.dev/path1">
+            <span>Link 1</span>
+          </a>
+        </div>
+        <a href="http://blog.boot.dev/path2">Link 2</a>
+      </body>
+    </html>
+  `;
+  expect(getURLsFromHTML(html)).toEqual([
+    'http://blog.boot.dev/path1',
+    'http://blog.boot.dev/path2',
+  ]);
+});
+
+test('ignores anchor tags without href attribute', () => {
+  const html = `
+    <html>
+      <body>
+        <a>Link without href</a>
+        <a href="http://blog.boot.dev/path">Link with href</a>
+      </body>
+    </html>
+  `;
+  expect(getURLsFromHTML(html)).toEqual(['http://blog.boot.dev/path']);
+});
+
+test('handles self-closing anchor tags', () => {
+  const html = `
+    <html>
+      <body>
+        <a href="http://blog.boot.dev/path1" />
+        <a href="http://blog.boot.dev/path2"></a>
+      </body>
+    </html>
+  `;
+  expect(getURLsFromHTML(html)).toEqual([
+    'http://blog.boot.dev/path1',
+    'http://blog.boot.dev/path2',
+  ]);
+});
+
+test('returns an empty array when no anchor tags are found', () => {
+  const html = `
+    <html>
+      <body>
+        <p>Some text</p>
+        <div>More text</div>
+      </body>
+    </html>
+  `;
+  expect(getURLsFromHTML(html)).toEqual([]);
+});
