13b66b0a93103357eac4a9e5ac8ffc60d63869b4
jacklion710 <jack.lion710@gmail.com>
2024-04-04 00:24:18 -0400

    crawlPage() function implmentation

--- crawl.js
+++ crawl.js
@@ -32,7 +32,44 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls
 }
 
+async function crawlPage(baseUrl, currentUrl, pages) {
+    const baseUrlObj = new URL(baseUrl)
+    const currentUrlObj = new URL(currentUrl)
+
+    if (baseUrlObj.hostname !== currentUrlObj.hostname) {
+        return pages
+    }
+    const normalizeCurrentUrl = normalizeURL(currentUrl)
+    if (pages[normalizedCurrentUrl] > 0) {
+        pages[normalizedCurrentUrl]++
+        return pages
+    }
+    pages[normalizedCurrentUrl] = 1
+    console.log(`Currently crawling: $currentUrl`)
+    try {
+        const resp = await fetch(currentUrl)
+        if(resp.status > 399) {
+            console.log('Error in fetching with status code: ', resp.status, ' on page: ', currentUrl)
+            return pages
+        }
+        const contentType = resp.headerrs.get('content-type')
+        if (!contentType.includes('text/html')) {
+            console.log("Non HTML response content type: ", contentType, " on page: ", currentUrl)
+            return pages
+        }
+        const htmlBody = await resp.text()
+        const nextUrls = getURLsFromHTML(htmlBody, baseUrl)
+        for (const nextUrl of nextUrls) {
+            pages = await crawlPage(baseUrl, nextUrl, pages)
+        }
+    } catch(err) {
+        console.log(`Error fetching from: `, currentUrl, err.message)
+    }
+    return pages
+}
+
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
